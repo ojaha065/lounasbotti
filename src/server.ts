1ea6f30db95841c89ca1a827c4ba62c41bcd99ab
Jani Haiko <haiko.jani@gmail.com>
2021-09-23 20:11:42 +0300

    Added home tab support

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,8 @@ import { LounasDataProvider, LounasResponse } from "model/LounasDataProvider.js"
 import RuokapaikkaFiDataProvider from "./model/RuokapaikkaFiDataProvider.js";
 import { Restaurant, RestaurantNameMap, Settings } from "./model/Settings.js";
 
-console.info("Lounasbotti server starting...");
+const VERSION = "1.0.3";
+console.info(`Lounasbotti v${VERSION} server starting...`);
 
 process.on("unhandledRejection", error => {
 	throw error;
@@ -88,6 +89,69 @@ app.message(/!(lounas|ruokaa)/, async ({say}) => {
 			}
 		}]
 	});
+});
+
+// Home tab
+app.event("app_home_opened", async ({client, event}) => {
+	try {
+		await client.views.publish({
+			user_id: event.user,
+			view: {
+				type: "home",
+				blocks: [
+					{
+						type: "header",
+						text: {
+							type: "plain_text",
+							text: `Lounasbotti V${VERSION}`
+						}
+					},
+					{
+						type: "section",
+						text: {
+							type: "mrkdwn",
+							text: "_By @Jani_"
+						}
+					},
+					{
+						type: "divider"
+					},
+					{
+						type: "section",
+						text: {
+							type: "mrkdwn",
+							text: `Tervehdys <@${event.user}>, nimeni on Lounasbotti. Kutsu minua komennoilla *!lounas* tai *!ruokaa* millä tahansa kanavalla, jonne minut on kutsuttu ja haen päivän lounaslistat valonnopeudella! _...tai ainakin yritän..._`
+						}
+					},
+					{
+						type: "section",
+						text: {
+							type: "plain_text",
+							text: "Voit myös avata yksityisen chatin kanssani ja käyttää edellä mainittuja komentoja siellä."
+						}
+					},
+					{
+						type: "section",
+						text: {
+							type: "mrkdwn",
+							text: "_Olen vielä beta-versio, mutta voit auttaa minua kehittymään paremmaksi_ -->"
+						},
+						accessory: {
+							type: "button",
+							text: {
+								type: "plain_text",
+								text: ":link: GitHub",
+								emoji: true
+							},
+							url: "https://github.com/ojaha065/lounasbotti"
+						}
+					}
+				]
+			}
+		});
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 const botPort = 3000;
