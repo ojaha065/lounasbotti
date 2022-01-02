5f357ad3956d6e7ca913edb7bf4560dfffcf7321
Jani Haiko <haiko.jani@gmail.com>
2022-01-02 13:43:09 +0200

    Use slack-block-builder

--- src/BlockParsers.ts
+++ src/BlockParsers.ts
@@ -1,43 +1,72 @@
 import bolt from "@slack/bolt";
+import { Job } from "node-schedule";
+import { Blocks, Elements, HomeTab, Md, user } from "slack-block-builder";
+import { SlackHomeTabDto } from "slack-block-builder/dist/internal";
 
-import { LounasResponse } from "./model/LounasDataProvider.js";
+import { LounasDataProvider, LounasResponse } from "./model/LounasDataProvider.js";
 import { RestaurantNameMap, Settings } from "./model/Settings.js";
 
-const parseLounasBlock = (lounasResponse: LounasResponse, settings: Settings, voting = true): bolt.Block | bolt.KnownBlock => {
-	const lounasBlock: (bolt.Block | bolt.KnownBlock) = {
-		type: "section",
-		text: {
-			type: "mrkdwn",
-			text: `*${RestaurantNameMap[lounasResponse.restaurant]}*\n${((lounasResponse.items || [lounasResponse.error]).map(item => `  ${getEmojiForLounasItem(item?.toString(), settings)} ${item}`).join("\n"))}`
-		},
-	};
-
-	if (voting && lounasResponse.items) {
-		lounasBlock.accessory = {
-			type: "button",
+export default class BlockParsers {
+	// TODO: Use slack-block-builder
+	public static parseLounasBlock(lounasResponse: LounasResponse, settings: Settings, voting = true): bolt.Block | bolt.KnownBlock {
+		const lounasBlock: (bolt.Block | bolt.KnownBlock) = {
+			type: "section",
 			text: {
-				type: "plain_text",
-				text: ":thumbsup:",
-				emoji: true
+				type: "mrkdwn",
+				text: `*${RestaurantNameMap[lounasResponse.restaurant]}*\n${((lounasResponse.items || [lounasResponse.error]).map(item => `  ${BlockParsers.getEmojiForLounasItem(item?.toString(), settings)} ${item}`).join("\n"))}`
 			},
-			value: `upvote-${lounasResponse.restaurant}`,
-			action_id: "upvoteButtonAction"
 		};
+	
+		if (voting && lounasResponse.items) {
+			lounasBlock.accessory = {
+				type: "button",
+				text: {
+					type: "plain_text",
+					text: ":thumbsup:",
+					emoji: true
+				},
+				value: `upvote-${lounasResponse.restaurant}`,
+				action_id: "upvoteButtonAction"
+			};
+		}
+	
+		return lounasBlock;
 	}
 
-	return lounasBlock;
-};
+	public static parseHomeTabView(data: { settings: Settings, version: string, userId: string, restartJob: Job | undefined, prefetchJob: Job | undefined }): Readonly<SlackHomeTabDto> {
+		const debugInformation: string[] = [
+			data.settings.configSource ? `Config loaded from ${data.settings.configSource}` : null,
+			`Data provider: ${(data.settings.dataProvider as LounasDataProvider).id} (${(data.settings.dataProvider as LounasDataProvider).baseUrl})`,
+			`[LounasEmoji] ${data.settings.emojiRules?.size ? `${data.settings.emojiRules?.size} regular expressions successfully loaded` : "No rules loaded"}`,
+			data.restartJob ? `Next scheduled restart is at ${data.restartJob.nextInvocation().toLocaleString("en-US")}` : null,
+			data.prefetchJob ? `Next data prefetching will occur at ${data.prefetchJob.nextInvocation().toLocaleString("en-US")}` : null
+		].filter(Boolean) as string[];
+	
+		return HomeTab()
+			.blocks(
+				Blocks.Header({ text: `Lounasbotti V${data.version}` }).end(),
+				Blocks.Section({ text: Md.italic("By <https://github.com/ojaha065|Jani Haiko>") }).end(),
+				Blocks.Divider().end(),
+				Blocks.Section({ text: `Tervehdys ${user(data.userId)}, nimeni on Lounasbotti. Kutsu minua komennoilla ${Md.bold("!lounas")} tai ${Md.bold("!ruokaa")} millä tahansa kanavalla, jonne minut on kutsuttu ja haen päivän lounaslistat valonnopeudella! ${Md.italic("...tai ainakin yritän...")}` }).end(),
+				Blocks.Section({ text: "Voit myös avata yksityisen chatin kanssani ja käyttää edellä mainittuja komentoja siellä." }).end(),
+				Blocks.Section({ text: `UUTTA: Kokeile myös komentoa ${Md.bold("!ruokaa huomenna")}` }).end(),
+				Blocks.Section({ text: `${Md.italic("Auta minua kehittymään paremmaksi")} ${Md.emoji("arrow_right")}` })
+					.accessory(Elements.Button({ actionId: "githubButtonLinkAction", text: `${Md.emoji("link")} GitHub`, url: data.settings.gitUrl }))
+					.end(),
+				Blocks.Context().elements(debugInformation.length ? `Debug information:\n${debugInformation.join("\n")}` : "---").end()
+			)
+			.buildToObject();
+	}
 
-export { parseLounasBlock };
-
-function getEmojiForLounasItem(lounasItem = "", settings: Settings): string {
-	if (settings.emojiRules) {
-		for (const [key, value] of settings.emojiRules) {
-			if (key.test(lounasItem)) {
-				return value;
+	private static getEmojiForLounasItem(lounasItem = "", settings: Settings) {
+		if (settings.emojiRules) {
+			for (const [key, value] of settings.emojiRules) {
+				if (key.test(lounasItem)) {
+					return value;
+				}
 			}
 		}
+	
+		return ":grey_question:";
 	}
-
-	return ":grey_question:";
 }
--- src/BotEvents.ts
+++ src/BotEvents.ts
@@ -1,13 +1,13 @@
 import bolt from "@slack/bolt";
 import { Job, scheduleJob, Range } from "node-schedule";
 
-import * as BlockParsers from "./BlockParsers.js";
 import * as Utils from "./Utils.js";
 
 import { LounasDataProvider, LounasResponse } from "./model/LounasDataProvider.js";
 import { Restaurant, RestaurantNameMap, Settings } from "./model/Settings.js";
 
 import * as LounasRepository from "./model/LounasRepository.js";
+import BlockParsers from "./BlockParsers.js";
 
 const AUTO_TRUNCATE_TIMEOUT = 1000 * 60 * 60 * 6; // 6 hrs
 const TOMORROW_REQUEST_REGEXP = /huomenna|tomorrow/i;
@@ -40,6 +40,13 @@ const initEvents = (app: bolt.App, settings: Settings, dataProvider: LounasDataP
 	app.action("githubButtonLinkAction", async ({ack}) => {
 		console.debug("GitHub link opened!");
 		ack();
+	});
+
+	app.event("app_home_opened", async args => {
+		args.client.views.publish({
+			user_id: args.event.user,
+			view: BlockParsers.parseHomeTabView({settings, version, restartJob, prefetchJob, userId: args.event.user})
+		});
 	});
 
 	if (settings.additionalRestaurants?.length) {
@@ -247,90 +254,6 @@ const initEvents = (app: bolt.App, settings: Settings, dataProvider: LounasDataP
 		}
 	
 		return Promise.resolve();
-	});
-
-	app.event("app_home_opened", async args => {
-		const debugInforamtion: string[] = [
-			settings.configSource ? `Config loaded from ${settings.configSource}` : null,
-			`Data provider: ${(settings.dataProvider as LounasDataProvider).id} (${(settings.dataProvider as LounasDataProvider).baseUrl})`,
-			`[LounasEmoji] ${settings.emojiRules?.size ? `${settings.emojiRules?.size} regular expressions successfully loaded` : "No rules loaded"}`,
-			restartJob ? `Next scheduled restart is at ${restartJob.nextInvocation().toLocaleString("en-US")}` : null,
-			prefetchJob ? `Next data prefetching will occur at ${prefetchJob.nextInvocation().toLocaleString("en-US")}` : null
-		].filter(Boolean) as string[];
-
-		args.client.views.publish({
-			user_id: args.event.user,
-			view: {
-				type: "home",
-				blocks: [
-					{
-						type: "header",
-						text: {
-							type: "plain_text",
-							text: `Lounasbotti V${version}`
-						}
-					},
-					{
-						type: "section",
-						text: {
-							type: "mrkdwn",
-							text: "_By <https://github.com/ojaha065|Jani Haiko>_"
-						}
-					},
-					{
-						type: "divider"
-					},
-					{
-						type: "section",
-						text: {
-							type: "mrkdwn",
-							text: `Tervehdys <@${args.event.user}>, nimeni on Lounasbotti. Kutsu minua komennoilla *!lounas* tai *!ruokaa* millä tahansa kanavalla, jonne minut on kutsuttu ja haen päivän lounaslistat valonnopeudella! _...tai ainakin yritän..._`
-						}
-					},
-					{
-						type: "section",
-						text: {
-							type: "plain_text",
-							text: "Voit myös avata yksityisen chatin kanssani ja käyttää edellä mainittuja komentoja siellä."
-						}
-					},
-					{
-						type: "section",
-						text: {
-							type: "mrkdwn",
-							text: "UUTTA: Kokeile myös komentoa *!ruokaa huomenna*."
-						}
-					},
-					{
-						type: "section",
-						text: {
-							type: "mrkdwn",
-							text: "_Auta minua kehittymään paremmaksi_ -->"
-						},
-						accessory: {
-							type: "button",
-							text: {
-								type: "plain_text",
-								text: ":link: GitHub",
-								emoji: true
-							},
-							url: "https://github.com/ojaha065/lounasbotti",
-							action_id: "githubButtonLinkAction"
-						}
-					},
-					{
-						type: "context",
-						elements: [
-							{
-								type: "plain_text",
-								text: debugInforamtion.length ? `Debug information:\n${debugInforamtion.join("\n")}` : "---"
-							}
-						]
-					
-					}
-				]
-			}
-		});
 	});
 };
 
