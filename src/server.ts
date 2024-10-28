051ac2c312b8bfdaf516548975c6c1a4614ce1d1
Jani Haiko <haiko.jani@gmail.com>
2024-10-28 17:51:16 +0200

    Update Bolt to V4 // Removes socket mode

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 // Global
 global.LOUNASBOTTI_JOBS = {};
-global.LOUNASBOTTI_VERSION = process.env["npm_package_version"] ?? "1.10.9";
+global.LOUNASBOTTI_VERSION = process.env["npm_package_version"] ?? "1.10.10";
 global.LOUNASBOTTI_TO_BE_TRUNCATED = [];
 
 import * as Sentry from "@sentry/node";
@@ -25,6 +25,7 @@ import type { AddressInfo } from "net";
 import type { LounasResponse } from "./model/dataProviders/LounasDataProvider.js";
 
 import bolt from "@slack/bolt";
+import type { Block, KnownBlock } from "@slack/types";
 
 import mongoose from "mongoose";
 
@@ -36,21 +37,16 @@ import BotCommands from "./BotCommands.js";
 
 console.info(`Lounasbotti v${global.LOUNASBOTTI_VERSION} server starting...`);
 
-const lounasCache: Record<string, { data: LounasResponse[], blocks: (bolt.Block | bolt.KnownBlock)[] }> = {};
+const lounasCache: Record<string, { data: LounasResponse[], blocks: (Block | KnownBlock)[] }> = {};
 
 if (!process.env["SLACK_SECRET"]
 	|| !process.env["SLACK_TOKEN"]
-	|| (process.env["SLACK_SOCKET"] && !process.env["SLACK_APP_TOKEN"])
 	|| !process.env["SLACK_MONGO_URL"]) {
 	throw new Error("Missing required environment variable(s)");
 }
 
-const socketMode: boolean = process.env["SLACK_SOCKET"] as unknown as boolean || false;
-
 const configURLs: URL[] | undefined = process.env["SLACK_CONFIG_URL"]?.split(";").map(s => new URL(s));
 readAndParseSettings(process.env["SLACK_CONFIG_NAME"], configURLs).then(settings => {
-	const { App } = bolt;
-
 	mongoose.connect(decodeBase64(process.env["SLACK_MONGO_URL"] as string), {
 		socketTimeoutMS: 10000
 	}).then(() => {
@@ -58,42 +54,32 @@ readAndParseSettings(process.env["SLACK_CONFIG_NAME"], configURLs).then(settings
 		readInstanceSettings(settings);
 	});
 	
-	const appOptions: bolt.AppOptions = {
+	const app = new bolt.App({
 		signingSecret: process.env["SLACK_SECRET"] || "",
-		token: process.env["SLACK_TOKEN"] || ""
-	};
-	if (socketMode) {
-		appOptions.appToken = process.env["SLACK_APP_TOKEN"] || "";
-		appOptions.socketMode = true;
-	}
-
-	appOptions.customRoutes = [
-		// Health check
-		{
-			path: "/health-check",
-			method: ["GET"],
-			handler: (_req, res) => {
-				res.writeHead(200);
-				res.end("OK");
+		token: process.env["SLACK_TOKEN"] || "",
+		customRoutes: [
+			// Health check
+			{
+				path: "/health-check",
+				method: ["GET"],
+				handler: (_req, res) => {
+					res.writeHead(200);
+					res.end("OK");
+				}
 			}
-		}
-	];
-	
-	const app = new App(appOptions);
+		]
+	});
 
 	BotCommands(app, settings);
 	BotEvents.initEvents(app, settings);
 	BotActions(app, settings);
 	
-	const botPort = 3000;
-	const webPort: number = (process.env["PORT"] || 8080) as unknown as number;
-	const portToUse = socketMode ? botPort : webPort;
 	app.start({
-		port: portToUse,
+		port: (process.env["PORT"] || 8080) as unknown as number,
 		host: "0.0.0.0"
 	}).then(server => {
 		const address = server.address() as AddressInfo;
-		console.info(`Lounasbotti server with instanceId "${settings.instanceId}" started on ${address.address}:${address.port} (Mode: ${socketMode ? "SocketMode" : "HTTP"})`);
+		console.info(`Lounasbotti server with instanceId "${settings.instanceId}" started on ${address.address}:${address.port} (Mode: HTTP})`);
 	});
 });
 
